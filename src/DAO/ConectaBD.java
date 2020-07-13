4bf593a5c0f8e2f943b8c4549b494040f27f97ba
Henrique Gaeta <46508056+rykehg@users.noreply.github.com>
2020-07-13 11:50:53 -0300

    Update ConectaBD.java

--- src/DAO/ConectaBD.java
+++ src/DAO/ConectaBD.java
@@ -7,9 +7,9 @@ public class ConectaBD {
     public static Connection conectabd() throws ClassNotFoundException{
         try{
             Class.forName("org.postgresql.Driver");
-            String user = "glbwgyiizfzmbi";
-            String password = "0156ecb27d7906ebb9984e1626d46a0d56dd1fde576d1ccb007c378c5f2096fb";
-            Connection con = DriverManager.getConnection("jdbc:postgresql://ec2-23-21-94-99.compute-1.amazonaws.com:5432/d7hacmrhir1hk7?sslmode=require", user, password);
+            String user = "YourUserHere";
+            String password = "YourPasswordHere";
+            Connection con = DriverManager.getConnection("jdbc:postgresql://YourPostgressURLHere?sslmode=require", user, password);
             return con;
         }
         catch(SQLException error){
